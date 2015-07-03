3f8205cf6424c0d26bd87c75efc9038ec46c73f7
Ole Henrik Skogstrøm <ole.henrik@hyper.no>
2015-07-03 16:52:57 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('sass', function () {
+  gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(sourcemaps.init())
+      .pipe(sass())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+gulp.task('default', function() {
+  gulp.start(['bower','watch']);
+});
---
+++ js/index.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+  var g = Snap.select('#Page-1');
+  g.drag();
+});
